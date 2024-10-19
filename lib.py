Fixed negative sign issue stemming from an incorrect direction of the flywheel moment vector. The original model seems correct

lib.py
@@ -23,7 +23,7 @@ def importDatafile(path, poles = 12):
 
     # Flywheel angular velocity in rad/s
     df["omegaFlywheel"] = df["erpm[0]"].values * 100 / (poles / 2) * 2 * math.pi / 60
-    flywheelOmega = np.array([np.zeros(len(df)), np.zeros(len(df)), df["omegaFlywheel"].values]).T
+    flywheelOmega = np.array([np.zeros(len(df)), np.zeros(len(df)), -df["omegaFlywheel"].values]).T
 
     omega = np.array([df["gyroADC[0]"].values,
                        df["gyroADC[1]"].values,
@@ -152,7 +152,7 @@ def computeI(angular_velocities, angular_accelerations, flywheel_angular_velocit
         zeta = np.matrix([zeta_X, zeta_Y, zeta_Z]).reshape((3, 6))
 
         global Jflywheel
-        beta = np.cross(omega, Jflywheel * flywheel_omega) + Jflywheel * flywheel_omega_dot
+        beta = -np.cross(omega, Jflywheel * flywheel_omega) - Jflywheel * flywheel_omega_dot
         B.extend(beta)
 
         ATA += np.matmul(zeta.T, zeta)
@@ -254,8 +254,8 @@ def simulateThrow(inertiaTensor, times, omega_0, flywheel_omegas, flywheel_omega
         for t in np.arange(t_1, t_2, ddt):
             # Simulate by solving the Euler rotation equation for the angular acceleration and using it
             # to numerically integrate the angular velocity
-            omega_dot = np.matmul(inv, -np.cross(omega, np.matmul(inertiaTensor, omega)) +
-                                  np.cross(omega, flywheel_angular_momentum) + flywheel_angular_momentum_dot)
+            omega_dot = np.matmul(inv, -np.cross(omega, np.matmul(inertiaTensor, omega)) -
+                                  np.cross(omega, flywheel_angular_momentum) - flywheel_angular_momentum_dot)
             omega = omega + omega_dot * ddt
         omegas.append(omega)
     omegas = np.array(omegas)

main.py
@@ -1,7 +1,7 @@
 from lib import *
 import lib
 
-df, omegas, accelerations, times, flywheel_omegas = importDatafile("gyro_spinning.csv")
+df, omegas, accelerations, times, flywheel_omegas = importDatafile("more_spin_with_flywheel.csv")
 
 # Prepare discrete filter coefficients
 filter_cutoff = 70
@@ -25,7 +25,7 @@ absolute_jerks = np.sqrt(jerks[:,0] ** 2 + jerks[:,1] ** 2 + jerks[:,2] ** 2)
 
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex="col", gridspec_kw={'height_ratios': [3, 1]})
 timePlotVector(times, filtered_omegas, ax=ax1, label="Filtered angular velocity", ylabel="Angular velocity (rad/s)")
-timePlotVector(times, filtered_flywheel_omegas, ax=ax2, label="Filtered flywheel angular velocity", ylabel="Flywheel angular velocity (rad/s)")
+timePlotVector(times, -filtered_flywheel_omegas, ax=ax2, label="Filtered flywheel angular velocity", ylabel="Flywheel angular velocity (rad/s)")
 
 # timePlotVector(times, omegas, ax=ax1, label="Angular velocity", ylabel="Angular velocity (rad/s)")
 # timePlotVector(times, omega_dots, ax=ax1, label="Angular acceleration", linestyle="dashed", alpha=0.7)