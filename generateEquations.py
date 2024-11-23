d4744b0e035d9b136e343fa9621b05c6779f22b3
Mike Kuijper <mike.m.kuijper@gmail.com>
2024-11-23 12:27:02 +0100

    Added flywheel jacobian equation

--- generateEquations.py
+++ generateEquations.py
@@ -9,11 +9,18 @@ def symprint(var, name):
 
 wx, wy, wz, dwx, dwy, dwz = symbols("wx wy wz dwx dwy dwz")
 Ixx, Ixy, Iyy, Ixz, Iyz, Izz = symbols("Ixx Ixy Iyy Ixz Iyz Izz")
+Jxx, Jxy, Jyy, Jxz, Jyz, Jzz = symbols("Jxx Jxy Jyy Jxz Jyz Jzz")
 
 w = Matrix([wx, wy, wz])
 dw = Matrix([dwx, dwy, dwz])
 i = Matrix([Ixx, Ixy, Iyy, Ixz, Iyz, Izz])
 I = Matrix([[Ixx, Ixy, Ixz], [Ixy, Iyy, Iyz], [Ixz, Iyz, Izz]])
+
+wfx, wfy, wfz, dwfx, dwfy, dwfz = symbols("wfx wfy wfz dwfx dwfy dwfz")
+wf = Matrix([wfx, wfy, wfz])
+dwf = Matrix([dwfx, dwfy, dwfz])
+J = Matrix([[Jxx, Jxy, Jxz], [Jxy, Jyy, Jyz], [Jxz, Jyz, Jzz]])
+j = Matrix([Jxx, Jxy, Jyy, Jxz, Jyz, Jzz])
 
 # torque-free equations of motion, left hand side
 lhs = I @ dw  +  w.cross(I @ w)
@@ -22,3 +29,9 @@ lhs = I @ dw  +  w.cross(I @ w)
 zetai = lhs.jacobian(i)
 symprint(zetai, "zeta_i")
 
+
+etai = (-J @ dwf - w.cross(J @ wf)).jacobian(j)
+symprint(etai, "eom")
+
+flywheel_inertia = -zetai @ i @ etai.inv()
+symprint(flywheel_inertia, "flywheel_inertia")
