d33e63b655b1a8538ccdd9c8babd782966ccea24
Mike Kuijper <mike.m.kuijper@gmail.com>
2024-12-13 17:24:42 +0100

    New test data dump

---
+++ input/cyberzoo_tests_the_second/config_c/groundtruth.py
@@ -0,0 +1,15 @@
+import numpy as np
+import lib
+
+# CONFIGURATION 03: proper heavy flywheel, larger motor
+
+m_dev = 0.10067  # [kg]
+
+grid = np.array([
+    [1, 0, 1, 0, 0, 1, 0, 1],
+    [0, 1, 0, 1, 1, 0, 1, 0],
+    [0, 1, 0, 1, 1, 0, 1, 0],
+    [1, 0, 1, 0, 0, 1, 0, 1]
+], dtype=bool)
+m, _, trueInertia = lib.calcGridObject(grid)
+m_obj = m
