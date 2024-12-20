eb18e24867e8fd0befcd1ec68f1b6f3419da3f43
Till Blaha <28341894+tblaha@users.noreply.github.com>
2024-12-20 17:26:23 +0100

    accuracy 6% worst case

--- analyse_individually.py
+++ analyse_individually.py
@@ -4,11 +4,12 @@ import os
 import pathlib
 import calibrate
 
-LOGFILE_PATH = "cyberzoo_tests_the_second/config_c"
+LOGFILE_PATH = "cyberzoo_tests_the_second/config_a"
 LOGFILES_ROOT = "input"
 SAVE_FOR_PUBLICATION = False
+doPlotting = False
 
-LP_CUTOFF = 100
+LP_CUTOFF = 20
 throw_offset = 300
 
 new_motor = True
@@ -18,8 +19,10 @@ j, _, __, I_dev, x_dev = calibrate.calibrateFlywheel(
                             dirlist=["device", "calibration"],
                             GROUNDTRUTH_PATH="calibration",
                             new_motor=new_motor,
+                            filter_cutoff=LP_CUTOFF,
                             )
 
+eigval_errs = []
 for (dirpath, dirnames, filenames) in os.walk(os.path.join(LOGFILES_ROOT, LOGFILE_PATH)):
     for f in filenames:
         if ".py" in f:
@@ -28,58 +31,26 @@ for (dirpath, dirnames, filenames) in os.walk(os.path.join(LOGFILES_ROOT, LOGFIL
         df, omegas, accelerations, times, flywheel_omegas \
             = importDatafile(os.path.join(LOGFILES_ROOT, LOGFILE_PATH, f), new_motor=new_motor)
 
-        # Prepare discrete filter coefficients
-        dt = (times[-1] - times[0]) / len(times)
+        filtered_accelerations, filtered_omegas, filtered_flywheel_omegas, \
+            jerks, omega_dots, flywheel_omega_dots, \
+            absolute_accelerations, absolute_omegas, absolute_jerks \
+            = signalChain(accelerations, omegas, flywheel_omegas, times, LP_CUTOFF)
 
-        filtered_accelerations = filterVectorSignalButterworth(accelerations, LP_CUTOFF, dt)
-        # Apply filter to data
-        # filtered_omegas = omegas
-        # filtered_flywheel_omegas = flywheel_omegas
-        filtered_flywheel_omegas = filterVectorSignalButterworth(flywheel_omegas, LP_CUTOFF, dt)
-        filtered_accelerations = filterVectorDynamicNotch(filtered_accelerations,
-                                                          filtered_flywheel_omegas[:, 2] / (2 * math.pi),
-                                                          10, #2.27
-                                                          dt)
-        # filtered_accelerations = filterVectorDynamicNotch(filtered_accelerations,
-        #                                                   filtered_flywheel_omegas[:, 2] / (math.pi),
-        #                                                   50,
-        #                                                   dt)
-        filtered_omegas = filterVectorSignalButterworth(omegas, LP_CUTOFF, dt)
-        filtered_flywheel_omegas = filterVectorSignalButterworth(flywheel_omegas, LP_CUTOFF, dt)
+        if doPlotting:
+            # # Initialise plot
+            fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex="col", gridspec_kw={'height_ratios': [2, 1, 1]})
+            timePlotVector(times, omegas, ax=ax1, label="Measured", ylabel=r"${\omega}$ (s$^{-1}$)", alpha=0.4)
+            timePlotVector(times, filtered_omegas, ax=ax1, label="Filtered")
+            # timePlotVector(times, omega_dots, ax=ax1, label="Angular acceleration", linestyle="dashed", alpha=0.8)
 
-        # Numerically differentiate filtered signals
-        jerks = differentiateVectorSignal(accelerations, dt)
-        omega_dots = differentiateVectorSignal(omegas, dt)
-        flywheel_omega_dots = differentiateVectorSignal(flywheel_omegas, dt)
+            timePlotVector(times, accelerations, ax=ax2, label="Measured", ylabel="Acceleration (ms$^{-1}$)", alpha=0.4)
+            timePlotVector(times, filtered_accelerations, ax=ax2, label="Filtered")
 
-        omega_dots = filterVectorSignalButterworth(omega_dots, LP_CUTOFF, dt)
-        flywheel_omega_dots = filterVectorSignalButterworth(flywheel_omega_dots, LP_CUTOFF, dt)
-
-        # Find lengths of filtered values
-        absolute_accelerations = np.sqrt(accelerations[:,0] ** 2 +
-                                         accelerations[:,1] ** 2 +
-                                         accelerations[:,2] ** 2)
-        absolute_omegas = np.sqrt(omegas[:,0] ** 2 +
-                                  omegas[:,1] ** 2 +
-                                  omegas[:,2] ** 2)
-        absolute_jerks = np.sqrt(jerks[:,0] ** 2 +
-                                 jerks[:,1] ** 2 +
-                                 jerks[:,2] ** 2)
-
-        # # Initialise plot
-        fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex="col", gridspec_kw={'height_ratios': [2, 1, 1]})
-        timePlotVector(times, omegas, ax=ax1, label="Measured", ylabel=r"${\omega}$ (s$^{-1}$)", alpha=0.4)
-        timePlotVector(times, filtered_omegas, ax=ax1, label="Filtered")
-        # timePlotVector(times, omega_dots, ax=ax1, label="Angular acceleration", linestyle="dashed", alpha=0.8)
-
-        timePlotVector(times, accelerations, ax=ax2, label="Measured", ylabel="Acceleration (ms$^{-1}$)", alpha=0.4)
-        timePlotVector(times, filtered_accelerations, ax=ax2, label="Filtered")
-
-        timePlotVector(times, flywheel_omegas, ax=ax3, label="Measured", ylabel=r"${\omega}_f$ (s$^{-1}$)", alpha=0.4)
-        timePlotVector(times, filtered_flywheel_omegas, ax=ax3, label="Filtered", ylabel=r"${\omega}_f$ (s$^{-1}$)")
-        # timePlotVector(times, filtered_flywheel_omegas / (2 * math.pi), ax=ax3, label="Filtered", ylabel=r"${\omega}_f$ (s$^{-1}$)")
-        timePlotVector(times, flywheel_omega_dots, ax=ax3, label="Flywheel angular acceleration", linestyle="dashed", alpha=0.8)
-        ax3.invert_yaxis()
+            timePlotVector(times, flywheel_omegas, ax=ax3, label="Measured", ylabel=r"${\omega}_f$ (s$^{-1}$)", alpha=0.4)
+            timePlotVector(times, filtered_flywheel_omegas, ax=ax3, label="Filtered", ylabel=r"${\omega}_f$ (s$^{-1}$)")
+            # timePlotVector(times, filtered_flywheel_omegas / (2 * math.pi), ax=ax3, label="Filtered", ylabel=r"${\omega}_f$ (s$^{-1}$)")
+            timePlotVector(times, flywheel_omega_dots, ax=ax3, label="Flywheel angular acceleration", linestyle="dashed", alpha=0.8)
+            ax3.invert_yaxis()
 
         starts, ends = detectThrow(times, absolute_omegas, absolute_accelerations, absolute_jerks, flywheel_omegas)
 
@@ -110,51 +81,58 @@ for (dirpath, dirnames, filenames) in os.walk(os.path.join(LOGFILES_ROOT, LOGFIL
         print(I_test)
         print(I_obj)
 
-        computeError(I_obj, groundtruth.trueInertia)
+        eigval_error, psi = computeError(I_obj, groundtruth.trueInertia)
+        eigval_errs.append(eigval_error)
         print(groundtruth.trueInertia)
         
         del groundtruth
         sys.path.remove(os.path.join(LOGFILES_ROOT, LOGFILE_PATH))
 
-        simulation_omegas = simulateThrow(I_test,
-                                          times[starts[0]+throw_offset:],
-                                          filtered_omegas[starts[0]+throw_offset],
-                                          filtered_flywheel_omegas[starts[0]+throw_offset:],
-                                          flywheel_omega_dots[starts[0]+throw_offset:])
-        timePlotVector(times[starts[0]+throw_offset+1:], simulation_omegas, label="Simulated", ax=ax1, linestyle="dashed", alpha=0.8)
+        if doPlotting:
+            simulation_omegas = simulateThrow(I_test,
+                                              times[starts[0]+throw_offset:],
+                                              filtered_omegas[starts[0]+throw_offset],
+                                              filtered_flywheel_omegas[starts[0]+throw_offset:],
+                                              flywheel_omega_dots[starts[0]+throw_offset:])
+            timePlotVector(times[starts[0]+throw_offset+1:], simulation_omegas, label="Simulated", ax=ax1, linestyle="dashed", alpha=0.8)
 
-        ax2.get_legend().remove()
-        ax3.get_legend().remove()
+            ax2.get_legend().remove()
+            ax3.get_legend().remove()
 
-        for s in starts:
-            ax1.axvline([times[s + throw_offset] * 1e3], linestyle="dashed", color="gray")
-        for e in ends:
-            ax1.axvline([times[e + throw_offset] * 1e3], linestyle="dotted", color="darkgray")
+            for s in starts:
+                ax1.axvline([times[s + throw_offset] * 1e3], linestyle="dashed", color="gray")
+            for e in ends:
+                ax1.axvline([times[e + throw_offset] * 1e3], linestyle="dotted", color="darkgray")
 
-        ax1.set_xlim([times[starts[0]] * 1e3, times[-1] * 1e3])
+            ax1.set_xlim([times[starts[0]] * 1e3, times[-1] * 1e3])
 
-        ax1.grid()
-        ax2.grid()
+            ax1.grid()
+            ax2.grid()
 
-        filename = os.path.splitext(os.path.join("output", LOGFILE_PATH, f))[0] + "-simulation.pdf"
-        pathlib.Path(os.path.dirname(filename)).mkdir(parents=True, exist_ok=True)
+            filename = os.path.splitext(os.path.join("output", LOGFILE_PATH, f))[0] + "-simulation.pdf"
+            pathlib.Path(os.path.dirname(filename)).mkdir(parents=True, exist_ok=True)
 
-        plt.savefig(filename, transparent=True, dpi=300, format="pdf", bbox_inches="tight")
-        if SAVE_FOR_PUBLICATION:
-            filename = os.path.splitext(os.path.join("output", LOGFILE_PATH, f))[0] + "-simulation_publication.pdf"
-            fig.set_size_inches(10, 2.5)
             plt.savefig(filename, transparent=True, dpi=300, format="pdf", bbox_inches="tight")
-        else:
-            # formatTicks(100, 20)
-            plt.tight_layout(pad=0.1)
-            # plt.subplots_adjust(left=0.06, right=0.88, top=0.95, bottom=0.05)
-            #manager = plt.get_current_fig_manager()
-            #manager.window.move(-1680, 0)
-            #manager.window.showMaximized()
+            if SAVE_FOR_PUBLICATION:
+                filename = os.path.splitext(os.path.join("output", LOGFILE_PATH, f))[0] + "-simulation_publication.pdf"
+                fig.set_size_inches(10, 2.5)
+                plt.savefig(filename, transparent=True, dpi=300, format="pdf", bbox_inches="tight")
+            else:
+                # formatTicks(100, 20)
+                plt.tight_layout(pad=0.1)
+                # plt.subplots_adjust(left=0.06, right=0.88, top=0.95, bottom=0.05)
+                #manager = plt.get_current_fig_manager()
+                #manager.window.move(-1680, 0)
+                #manager.window.showMaximized()
 
-            def on_resize(event):
-                fig.tight_layout()
-                fig.canvas.draw()
-            cid = fig.canvas.mpl_connect('resize_event', on_resize)
-            plt.show()
+                def on_resize(event):
+                    fig.tight_layout()
+                    fig.canvas.draw()
+                cid = fig.canvas.mpl_connect('resize_event', on_resize)
+                plt.show()
     break
+
+print()
+eigval_errs = np.asarray(eigval_errs)
+print(f"mean eigval error: {100*eigval_errs.mean()}%")
+print(f"max eigval error: {100*eigval_errs.max()}%")
