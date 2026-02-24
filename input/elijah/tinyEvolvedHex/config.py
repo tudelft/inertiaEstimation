cf48441ac4d020b46306181dce6d56138d4601b4
Till Blaha <28341894+tblaha@users.noreply.github.com>
2026-02-24 10:50:01 +0100

    elijah tiny hex

---
+++ input/elijah/tinyEvolvedHex/config.py
@@ -0,0 +1,8 @@
+# name and m_obj must be set, the rest can be None or unset
+# to compute error metrics, set I_obj, and optinally x_obj
+
+name = "tinyEvolvedHex"
+
+m_obj = 0.321
+x_obj = None
+I_obj = None
